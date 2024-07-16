d9135ca165d6796bb8fd3c9e31efe70fc72e1cd7
rob <rob@b7c.io>
2024-07-16 15:27:36 +1200

    fix additive blending with alpha channel in dst image

--- imager/drawer.go
+++ imager/drawer.go
@@ -9,16 +9,24 @@ import (
 type additiveDrawer struct{}
 
 func (additiveDrawer) Draw(dst draw.Image, r image.Rectangle, src image.Image, sp image.Point) {
+	sx, sy := src.Bounds().Min.X+sp.X, src.Bounds().Min.Y+sp.Y
 	sw, sh := src.Bounds().Dx(), src.Bounds().Dy()
 	for y := 0; y < r.Dy() && (sp.Y+y) < sh; y++ {
 		for x := 0; x < r.Dx() && (sp.X+x) < sw; x++ {
-			sr, sg, sb, _ := src.At(src.Bounds().Min.X+sp.X+x, src.Bounds().Min.Y+sp.Y+y).RGBA()
+			sr, sg, sb, _ := src.At(sx+x, sy+y).RGBA()
 			dr, dg, db, da := dst.At(r.Min.X+x, r.Min.Y+y).RGBA()
+
+			if da > 0 {
+				dr = (dr * 0xffff) / da
+				dg = (dg * 0xffff) / da
+				db = (db * 0xffff) / da
+			}
+
 			c := color.NRGBA64{
-				R: uint16(min(65535, dr+sr)),
-				G: uint16(min(65535, dg+sg)),
-				B: uint16(min(65535, db+sb)),
-				A: uint16(da),
+				R: uint16(min(0xffff, dr+sr)),
+				G: uint16(min(0xffff, dg+sg)),
+				B: uint16(min(0xffff, db+sb)),
+				A: uint16(min(0xffff, da)),
 			}
 			dst.Set(r.Min.X+x, r.Min.Y+y, c)
 		}
