22eb30028fc749c87fed3680a5362d29775f8f24
rob <rob@b7c.io>
2024-08-18 13:36:48 +1200

    add TextFixFiguredata

--- gamedata/origins/figuredata_test.go
+++ gamedata/origins/figuredata_test.go
@@ -21,3 +21,21 @@ func TestUnmarshalFiguredata(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestFixFiguredata(t *testing.T) {
+	var (
+		beforeFix        = `["M":["hr":[["s":1,"p":["sh":"2"],"c":["AAAAAA","BBBBBB","CCCCCC","DDDDDD"]]]]]`
+		expectedAfterFix = `{"M":{"hr":[{"s":1,"p":{"sh":"2"},"c":["AAAAAA","BBBBBB","CCCCCC","DDDDDD"]}]}}`
+	)
+
+	bytes := []byte(beforeFix)
+	err := fixFigureData(bytes)
+	if err != nil {
+		t.Fatal(err)
+	}
+	actualAfterFix := string(bytes)
+
+	if expectedAfterFix != actualAfterFix {
+		t.Fatalf("Fixed figure data not as expected\n  Expected: %q\n    Actual: %q", expectedAfterFix, actualAfterFix)
+	}
+}
