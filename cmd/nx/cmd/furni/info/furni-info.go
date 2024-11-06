feb2e81984a354b98e662fba8a2bf55b97601028
rob <rob@b7c.io>
2024-11-07 00:44:02 +1300

    add type option to furni info command

--- cmd/nx/cmd/furni/info/furni-info.go
+++ cmd/nx/cmd/furni/info/furni-info.go
@@ -1,10 +1,12 @@
 package info
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/spf13/cobra"
 
+	"xabbo.io/nx"
 	gd "xabbo.io/nx/gamedata"
 
 	_root "xabbo.io/nx/cmd/nx/cmd"
@@ -18,15 +20,20 @@ var Cmd = &cobra.Command{
 	RunE: runInfo,
 }
 
-var (
+var opts struct {
+	itemType   string
 	kind       int
 	identifier string
-)
+	json       bool
+}
 
 func init() {
 	f := Cmd.Flags()
-	f.IntVarP(&kind, "kind", "k", 0, "The furni kind (type ID)")
-	f.StringVarP(&identifier, "identifier", "i", "", "The furni identifier (class name)")
+
+	f.StringVarP(&opts.itemType, "type", "t", "", "The furni type (floor/wall)")
+	f.IntVarP(&opts.kind, "kind", "k", 0, "The furni kind (type ID)")
+	f.StringVarP(&opts.identifier, "identifier", "i", "", "The furni identifier (class name)")
+	f.BoolVar(&opts.json, "json", false, "Output JSON")
 
 	furni.Cmd.AddCommand(Cmd)
 }
@@ -47,16 +54,27 @@ func runInfo(cmd *cobra.Command, args []string) (err error) {
 		if furniInfo, ok := mgr.Furni()[identifier]; ok {
 			fi = furniInfo
 		}
-	} else if kind > 0 {
+	} else if opts.kind > 0 {
+		var itemType nx.ItemType
+		switch opts.itemType {
+		case "":
+			return fmt.Errorf("item type is required when kind is specified")
+		case "floor", "f", "s":
+			itemType = nx.ItemFloor
+		case "wall", "w", "i":
+			itemType = nx.ItemWall
+		default:
+			return fmt.Errorf("invalid item type: %q", opts.itemType)
+		}
 		for _, f := range mgr.Furni() {
-			if f.Kind == kind {
+			if f.Type == itemType && f.Kind == opts.kind {
 				fi = f
 				break
 			}
 		}
-	} else if identifier != "" {
+	} else if opts.identifier != "" {
 		for _, f := range mgr.Furni() {
-			if f.Identifier == identifier {
+			if f.Identifier == opts.identifier {
 				fi = f
 				break
 			}
@@ -64,7 +82,15 @@ func runInfo(cmd *cobra.Command, args []string) (err error) {
 	}
 
 	if fi != nil {
-		util.RenderFurniInfo(fi)
+		if opts.json {
+			utf8, err := json.Marshal(fi)
+			if err != nil {
+				return err
+			}
+			fmt.Print(string(utf8))
+		} else {
+			util.RenderFurniInfo(fi)
+		}
 	} else {
 		return fmt.Errorf("furni not found")
 	}
